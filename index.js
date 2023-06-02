342d07395c6ecc6a296f8a78e737dec5e1df9916
TangiresH <dimon4ik1010@gmail.com>
2023-06-02 05:21:33 +0300

    fixed an error with the tasks.json file
    Previously, when we saved the input, the task was not added to the list of the tasks.jason file, but replaced the old task. Bug found and fixed.

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const readline = require('readline');
 const nonInteractiveInput= require('./nonInteractiveMode');
 const interactiveInput = require('./interactiveInput');
 const { isInteractiveMode }= require('./operatingModes');
+const TaskTracker = require("./taskTracker");
+
 
 // Creating an interface for reading input from the console
 const rl = require('./rl');
-
+const taskTracker = new TaskTracker();
+taskTracker.loadFromFile();
 
 rl.on('line', (input) => {
   const [command, ...args] = input.trim().split(' ');
